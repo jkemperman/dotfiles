e0f847ff0ed007ce18fddaff8b27205894d73ce8
Joep Kemperman <kazooiej@gmail.com>
2013-12-27 14:39:07 +0100

    Added configuration file for vimperator.

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 # Declare variables
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="vimrc vim Xresources zshrc i3"
+files="vimrc vim Xresources zshrc i3 vimperatorrc"
 
 # Create dotfiles_old in home directory
 echo "Creating $olddir for backup of any existing dotfiles in ~..."
