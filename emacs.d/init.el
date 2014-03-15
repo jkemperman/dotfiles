c424768e92f57a95d73bb424590efcc967b54769
Joep Kemperman <kazooiej@gmail.com>
2014-03-15 14:55:05 +0100

    Enabled evil mode in emacs for modal editing and vim keybindings.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,12 @@
+;; Enable evil mode
+(add-to-list 'load-path "~/.emacs.d/evil/")
+	(require 'evil)
+	(evil-mode 1)
+
+;; Enable color theme
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'color-theme)
 	(color-theme-initialize)
 	(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/themes/")
 	(load "~/.emacs.d/lisp/themes/solarized-theme.el")
+
