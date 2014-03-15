fa548069008bde280bc1430d2bee1d313662b0e8
Joep Kemperman <kazooiej@gmail.com>
2014-03-15 14:41:23 +0100

    Moved some files.

---
+++ emacs.d/init.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'color-theme)
+	(color-theme-initialize)
+	(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/themes/")
+	(load "~/.emacs.d/lisp/themes/solarized-theme.el")
