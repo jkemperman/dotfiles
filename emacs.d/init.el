2f3a5823841a448b88b2e42250dfc332f6492cff
Joep Kemperman <mail@joepkemperman.nl>
2014-05-27 20:50:14 +0200

    Emacs now has solarized working, although the colors are slightly off.

---
+++ emacs.d/init.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/color-theme")
+(require 'color-theme)
+(eval-after-load "color-theme"
+	'(progn
+		(color-theme-initialize)
+		(color-theme-hober)))
+(color-theme-solarized-dark)
+(custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
