185c2d73ea4afb5efe3b97243d17e1c94053b67b
Joep Kemperman <mail@joepkemperman.nl>
2014-05-28 18:54:59 +0200

    Added some comments to init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,4 @@
+; Enable line numbers
 (require 'linum)
 (global-linum-mode 1)
 (setq linum-format "%d ")
@@ -10,8 +11,10 @@
 ; Change font for GUI
 (add-to-list 'default-frame-alist '(font . "Inconsolata-12"))
 
+; Enable solarized theme
 (add-to-list 'load-path "~/.emacs.d/color-theme")
 (require 'color-theme)
+
 (eval-after-load "color-theme"
 	'(progn
 		(color-theme-initialize)
@@ -20,7 +23,7 @@
 (defun my-color-theme-solarized-dark ()
 	(interactive)
 	(color-theme-solarized-dark)
-	(set-face-attribute 'fringe nil :background "#073642")
-	(set-face-attribute 'linum nil :background "#073642"))
+	(set-face-attribute 'fringe nil :background "black")
+	(set-face-attribute 'linum nil :background "black"))
 
 (my-color-theme-solarized-dark)
